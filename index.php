0c2e3e7b5f14f03dfaca6ae03eea50b8a05bdce5
Alexandr Dikarev <enotov21@yandex.ru>
2023-07-15 03:50:55 +0300

    2

--- index.php
+++ index.php
@@ -9,11 +9,10 @@
   </head>
   <body>
     <?php include 'layout/navbar.php' ?>
-
-
-
-
-
+    <div class="container-fluid">
+    	
+    </div>
+    <footer>123</footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
